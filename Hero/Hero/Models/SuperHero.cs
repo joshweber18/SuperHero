8df6c622a665ddb0fbb9329c0c29dbfc1c1a3f29
joshweber18 <josh18weber@gmail.com>
2018-11-29 10:31:35 -0600

    Created Contoller, View, Model

---
+++ Hero/Hero/Models/SuperHero.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+
+namespace Hero.Models
+{
+    public class SuperHero
+    {
+        [Key]
+        public string SuperHeroName { get; set; }
+        public string AlterEgo { get; set; }
+        public string PrimarySuperHeroAbility { get; set; }
+        public string SecondarySuperHeroAbility { get; set; }
+        public string CatchPhrase { get; set; }
+        
+    }
+}
